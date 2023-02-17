6c0ff004ffc7fdb923d3f9f4d3ff096ea74bd56a
jtfogarty <jeff@fogarty.org>
2023-02-17 14:29:59 -0600

    modal input form for access key

---
+++ frontend/src/lib/Modal/ModalExampleForm.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	// Props
+	/** Exposes parent props to this component. */
+	export let parent: any;
+
+	// Stores
+	import { modalStore } from '@skeletonlabs/skeleton';
+
+	// Form Data
+	const formData = {
+		name: '',
+		token: ''
+	};
+
+	// We've created a custom submit function to pass the response and close the modal.
+	function onFormSubmit(): void {
+		if ($modalStore[0].response) $modalStore[0].response(formData);
+		modalStore.close();
+	}
+
+	// Base Classes
+	const cBase = 'space-y-4';
+	const cForm = 'border border-surface-500 p-4 space-y-4 rounded-container-token';
+</script>
+
+<!-- @component This example creates a simple form modal. -->
+
+<div class="modal-example-form {cBase}">
+	<!-- Enable for debugging: -->
+	<!-- <pre>{JSON.stringify(formData, null, 2)}</pre> -->
+	<form class="modal-form {cForm}">
+		<label class="label">
+			<span>User Name</span>
+			<input class="input text-center" type="text" bind:value={formData.name} placeholder="Enter your user name..." />
+		</label>
+		<label class="label">
+			<span>GitLab Personal Access Token</span>
+			<input class="input text-center" type="text" bind:value={formData.token} placeholder="Enter GitLab Token..." />
+		</label>
+	</form>
+	<!-- prettier-ignore -->
+	<footer class="modal-footer {parent.regionFooter}">
+        <button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
+        <button class="btn {parent.buttonPositive}" on:click={onFormSubmit}>Submit</button>
+    </footer>
+</div>
